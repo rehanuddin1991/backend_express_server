9b7d61990d563361d722db4f35b1d6b9ba716477
rehanuddin1991 <rehan.ictd@gmail.com>
2024-10-05 17:08:30 +0600

    server with express

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express= require("express");
+const cors= require("cors");
 const courseDetails= require("./courseDetails.json");
- 
- 
-
 const app=express();
 const PORT=process.env.PORT || 5000;
+app.use(cors()); 
  
- 
- 
- 
+
 app.get("/"  , (req , res)=> {
     res.send(
-        [
-            {
-                id : 1,
-                year : 2001,
-            }
-            ,
-            {
-                id :2,
-                year : 2002,
-            }
-        ]
+        "congratutations! backend server running"
     );
 });
 
 app.get("/course"  , (req , res)=> {
-    res.send(courseDetails);
+    res.json(courseDetails);
 });
+
+ 
+
+app.get('/course/:id', (req, res)=>{
+    var singleCourse = courseDetails.filter(function(course){
+        let inputCourseId;
+        if(req.params.id.length==1){  inputCourseId="0" + req.params.id;}
+        else {  inputCourseId=req.params.id;}
+        
+       if(course.course_id == inputCourseId){
+          return true;
+       }
+    });
+    if(singleCourse.length == 1){
+       res.json(singleCourse[0])
+    } else {
+       
+       res.send({message: "Not Found"});
+    }
+ });
+
+
+
 
 const start = async ()=>
 {
